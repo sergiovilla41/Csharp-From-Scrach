078f177c4974ece74323c9cf15a14e06f18a8154
Sergio <serfusion41@gmail.com>
2023-09-07 12:26:53 -0500

    Midified Branch Csharp05

---
+++ HelloWorld/classes_and_inheritance/Program.cs
@@ -0,0 +1,52 @@
+﻿class Rectangulo
+{
+    
+    public double Base { get; set; }
+
+    
+    public double Altura { get; set; }
+
+    
+    public Rectangulo(double baseRectangulo, double alturaRectangulo)
+    {
+        Base = baseRectangulo;
+        Altura = alturaRectangulo;
+    }
+
+ 
+    public double CalcularArea()
+    {
+        return Base * Altura;
+    }
+
+
+    public double CalcularPerimetro()
+    {
+        return 2 * (Base + Altura);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+       
+        Console.Write("Ingrese la base del rectángulo: ");
+        double baseRectangulo = Convert.ToDouble(Console.ReadLine());
+
+        
+        Console.Write("Ingrese la altura del rectángulo: ");
+        double alturaRectangulo = Convert.ToDouble(Console.ReadLine());
+
+       
+        Rectangulo miRectangulo = new Rectangulo(baseRectangulo, alturaRectangulo);
+
+        
+        double area = miRectangulo.CalcularArea();
+        Console.WriteLine("Área del rectángulo: " + area);
+
+        
+        double perimetro = miRectangulo.CalcularPerimetro();
+        Console.WriteLine("Perímetro del rectángulo: " + perimetro);
+    }
+}
