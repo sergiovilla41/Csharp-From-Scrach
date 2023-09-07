30b2a62ca81627a2e0e9e3f7c571b8135df21ce0
Sergio <serfusion41@gmail.com>
2023-09-07 18:44:19 -0500

    Modified Buckle Fibonacci

---
+++ Buckles/Program.cs
@@ -0,0 +1,31 @@
+﻿class BK
+{
+    static void Main()
+    {
+        Console.Write("Ingrese  el número calcular: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        if (n <= 0)
+        {
+            Console.WriteLine("Por favor, ingrese un número válido mayor que cero.");
+            return;
+        }
+
+        long a = 0; // Primer término de la serie
+        long b = 1; // Segundo término de la serie
+
+        Console.WriteLine("Serie de Fibonacci:");
+
+        // Mostrar los primeros n términos de la serie de Fibonacci
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(a + " ");
+
+            long temp = a;
+            a = b;
+            b = temp + b;
+        }
+
+        Console.WriteLine();
+    }
+}
