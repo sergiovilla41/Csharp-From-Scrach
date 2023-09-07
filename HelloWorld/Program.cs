e857ec19533bef212c9f06e0799ddf45538db27a
Sergio <serfusion41@gmail.com>
2023-09-07 07:49:12 -0500

    Modified Branch Csharp03

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,17 +1,75 @@
-﻿namespace HelloWorld{
+﻿// See https://aka.ms/new-console-template for more information
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main()
+        {
+            string nombre = "";
+            DateTime fechaNacimiento = DateTime.MinValue;
 
-class program{
+           //Nombre
+            while (string.IsNullOrEmpty(nombre) || ContieneNumeros(nombre))
+            {
+                Console.Write("Ingresa tu nombre : ");
+                nombre = Console.ReadLine().Trim();
 
-static void Main(){
-var Name =  "Jhon";
-Name = "1Jhon";
-Console.WriteLine($"Hello World! {Name}");
-Console.WriteLine();
+                if (string.IsNullOrEmpty(nombre))
+                {
+                    Console.WriteLine("El nombre no puede estar vacío. Por favor, inténtalo de nuevo.");
+                }
+                else if (ContieneNumeros(nombre))
+                {
+                    Console.WriteLine("El nombre no puede contener números. Por favor, inténtalo de nuevo.");
+                }
+            }
 
+            //  fecha de nacimiento
+            while (fechaNacimiento == DateTime.MinValue)
+            {
+                Console.Write("Ingresa tu fecha de nacimiento (dd-mm-aa): ");
+                if (DateTime.TryParse(Console.ReadLine(), out fechaNacimiento))
+                {
+                    // Verificar la fecha de nacimiento 
+                    if (fechaNacimiento > DateTime.Now)
+                    {
+                        Console.WriteLine("La fecha de nacimiento no puede ser mayor a la fecha actual. Por favor, inténtalo de nuevo.");
+                        fechaNacimiento = DateTime.MinValue;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Formato de fecha incorrecto. Por favor, ingresa una fecha válida (dd-mm-aa).");
+                }
+            }
 
+            // edad
+            int edad = DateTime.Now.Year - fechaNacimiento.Year;
+
+            // Verificar si ya ha pasado el cumpleaños de este año
+            if (DateTime.Now.Month < fechaNacimiento.Month || (DateTime.Now.Month == fechaNacimiento.Month && DateTime.Now.Day < fechaNacimiento.Day))
+            {
+                edad--;
+            }
+
+            // Mostrar el resultado
+            Console.WriteLine($"Hola, {nombre}!");
+            Console.WriteLine($"Tu fecha de nacimiento es: {fechaNacimiento}.");
+            Console.WriteLine($"Tu edad es: {edad} años.");
+            Console.ReadLine();
+
+        }
+        static bool ContieneNumeros(string cadena)
+        {
+            foreach (char c in cadena)
+            {
+                if (char.IsDigit(c))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+    }
 }
-
-}
-
-}
-
